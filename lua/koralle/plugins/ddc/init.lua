a8ed222f1c1ba402e491802e1c4a63162e9d9877
koralle <mkhd7930@gmail.com>
2023-11-02 19:57:01 +0900

    chore(plugins): ddc-filter-sorter_rank, ddc-filter-matcher_head

--- lua/koralle/plugins/ddc/init.lua
+++ lua/koralle/plugins/ddc/init.lua
@@ -5,6 +5,8 @@ local spec = {
     dependencies = {
       "vim-denops/denops.vim",
       "Shougo/ddc-source-around",
+      "Shougo/ddc-filter-matcher_head",
+      "Shougo/ddc-filter-sorter_rank",
     },
     enabled = function()
       if vim.fn.executable("deno") == 1 then
@@ -14,7 +16,19 @@ local spec = {
       end
     end,
     config = function()
-      vim.fn["ddc#custom#patch_global"]("sources", {})
+      vim.fn["ddc#custom#patch_global"]("sources", {
+        "around",
+      })
+      vim.fn["ddc#custom#patch_global"]("sourceOptions", {
+        _ = {
+          matchers = {
+            "matcher_head",
+          },
+          sorters = {
+            "sorter_rank",
+          },
+        },
+      })
       -- vim.fn["ddc#custom#patch_global"]("sources", {})
       -- vim.fn["ddc#custom#patch_global"]("sourceOptions", {})
       -- vim.fn["ddc#custom#patch_global"]("sourceParams", {})
@@ -23,6 +37,14 @@ local spec = {
   },
   {
     "Shougo/ddc-source-around",
+    lazy = true,
+  },
+  {
+    "Shougo/ddc-filter-matcher_head",
+    lazy = true,
+  },
+  {
+    "Shougo/ddc-filter-sorter_rank",
     lazy = true,
   },
 }

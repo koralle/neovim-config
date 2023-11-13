0421bdae4ab620baeb21d8a57d2dd24b242fc859
koralle <mkhd7930@gmail.com>
2023-11-14 00:07:41 +0900

    chore(ddu): first ddu-ui-filer settings

--- lua/koralle/plugins/ddu/init.lua
+++ lua/koralle/plugins/ddu/init.lua
@@ -4,7 +4,34 @@ local spec = {
     "Shougo/ddu.vim",
     dependencies = {
       "vim-denops/denops.vim",
+      "Shougo/ddu-ui-filer",
+      "Shougo/ddu-source-file",
+      "Shougo/ddu-kind-file",
+      "Shougo/ddu-column-filename",
     },
+    config = function()
+      vim.fn["ddu#custom#patch_global"]({
+        ui = "filer",
+        sources = {
+          {
+            name = "file",
+            params = {},
+          },
+        },
+        sourceOptions = {
+          _ = {
+            columns = {
+              "filename",
+            },
+          },
+        },
+        kindOptions = {
+          file = {
+            defaultAction = "open",
+          },
+        },
+      })
+    end,
   },
   {
     "Shougo/ddu-source-file",

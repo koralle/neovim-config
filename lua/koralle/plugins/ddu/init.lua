ee28c2dff2c090fcd94ecff5b59e8e1b54d099f0
koralle <mkhd7930@gmail.com>
2023-11-28 22:13:31 +0900

    chore(ddu): ui-params

--- lua/koralle/plugins/ddu/init.lua
+++ lua/koralle/plugins/ddu/init.lua
@@ -46,14 +46,27 @@ local spec = {
         },
       })
 
+      local lines = vim.opt.lines:get()
+      local height, row = math.floor(lines * 0.8), math.floor(lines * 0.1)
+      local columns = vim.opt.columns:get()
+      local width, col = math.floor(columns * 0.8), math.floor(columns * 0.1)
+
       vim.fn["ddu#custom#patch_local"]("ff", {
         ui = "ff",
         uiParams = {
           ff = {
             split = "floating",
             startFilter = true,
-            -- winWidth = math.floor(vim.o.columns * 0.8),
-            -- winHeight = math.floor(vim.o.columns * 0.6),
+            prompt = "> ",
+            winHeight = height,
+            winRow = row,
+            winWidth = width,
+            winCol = col,
+            floatingBorder = "single",
+            filterFloatingPosition = "top",
+            autoAction = {
+              name = "preview",
+            },
           },
         },
         sources = {

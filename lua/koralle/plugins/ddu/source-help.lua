7f7780f70478bc9e9c2d6c26a8ff7f3b5233a2f9
koralle <mkhd7930@gmail.com>
2024-01-23 10:15:02 +0900

    add: ddu/source-help

---
+++ lua/koralle/plugins/ddu/source-help.lua
@@ -0,0 +1,41 @@
+---@type LazySpec
+local spec = {
+  "matsui54/ddu-source-help",
+  name = "ddu-source-help",
+  dependencies = {
+    "ddu.vim",
+  },
+  init = function()
+    vim.keymap.set("n", "<space>hl", function()
+      vim.fn["ddu#start"]({
+        name = "help",
+        ui = "ff",
+      })
+    end)
+  end,
+  config = function()
+    local helpers = require("koralle.helpers.ddu")
+
+    helpers.patch_local("help", {
+      sources = {
+        {
+          name = "help",
+        },
+      },
+      sourceOptions = {
+        help = {
+          matchers = {
+            "matcher_substring",
+          },
+        },
+      },
+      kindOptions = {
+        help = {
+          defaultAction = "vsplit",
+        },
+      },
+    })
+  end,
+}
+
+return spec

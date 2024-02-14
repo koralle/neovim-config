02936ffb6d7b937a2ea572d42882e8ffbe4f3f66
koralle <mkhd7930@gmail.com>
2024-02-15 01:00:02 +0900

    fix(ddu): attach event `VimResized`

--- lua/koralle/plugins/ddu/init.lua
+++ lua/koralle/plugins/ddu/init.lua
@@ -24,6 +24,34 @@ local spec = {
           },
         },
       })
+
+      local function resize()
+        local lines = vim.opt.lines:get()
+        local height, row = math.floor(lines * 0.8), math.floor(lines * 0.1)
+        local columns = vim.opt.columns:get()
+        local width, col = math.floor(columns * 0.8), math.floor(columns * 0.1)
+        local previewWidth = math.floor(width / 2)
+
+        helpers.patch_global({
+          uiParams = {
+            ff = {
+              winHeight = height,
+              winRow = row,
+              winWidth = width,
+              winCol = col,
+              previewHeight = height,
+              previewRow = row,
+              previewWidth = previewWidth,
+              previewCol = col + (width - previewWidth),
+            },
+          },
+        })
+      end
+      resize()
+
+      vim.api.nvim_create_autocmd("VimResized", {
+        callback = resize,
+      })
     end,
   },
 }

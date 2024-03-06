1ffb08c6654ac277442b61e53519ac95b32e2e1d
koralle <mkhd7930@gmail.com>
2024-03-06 19:05:12 +0900

    feat(lsp): yamlls

---
+++ lua/koralle/plugins/lsp/yamlls.lua
@@ -0,0 +1,13 @@
+local server = {
+  settings = {
+    yaml = {
+      schemaStore = {
+        enable = false,
+        url = "",
+      },
+      schemas = require("schemastore").yaml.schemas(),
+    },
+  },
+}
+
+return server
